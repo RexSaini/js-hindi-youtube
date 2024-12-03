1e01d6668b2a20602bb8f3d91621167af66a279c
Rohan Kumar Saini <89375168+RexSaini@users.noreply.github.com>
2024-12-03 10:58:08 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") //can't be used in not JS but in browser
+
+/*data types like
+number
+bigint
+string =""
+boolean = true/false
+null = standalone value
+undefined 
+symbol => for uniqueness
+
+object
+*/
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
