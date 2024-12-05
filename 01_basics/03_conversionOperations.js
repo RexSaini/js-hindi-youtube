846c146a39565ba7fd0379974bbfc7a90c282d86
Rohan Kumar Saini <89375168+RexSaini@users.noreply.github.com>
2024-12-05 11:07:14 +0000

    operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -28,3 +28,26 @@ let someNumber = 33
 let stringNumber = String(someNumber) //Converts into String
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+// Operations
+
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "Hello"
+let str2 = " World"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2) //Output will be 12
+console.log("1" + 2 + 2) //Output will be 122
+console.log(1 + 2 + "2") //Output will be 32
+
+console.log(+true) //Output will be 1
+console.log(+"") //Output will be 0
+
+let gameCounter = 100
+gameCounter++ 
+console.log(gameCounter); // Output 101
